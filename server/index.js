8dbd16ebb6b2e1b895aa7a288635ec45559500b9
vipratripathi745 <vipratripathi745@gmail.com>
2026-07-29 19:40:51 +0530

    Clean repository and configure gitignore

--- server/index.js
+++ server/index.js
@@ -4,17 +4,36 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+import pool from "./config/db.js";
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("🚀 GoCart Backend is Running...");
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      success: true,
+      message: "🚀 Backend Running Successfully",
+      databaseTime: result.rows[0].now,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Database Connection Failed",
+      error: error.message,
+    });
+  }
 });
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}`);
+  console.log(`✅ Server running on http://localhost:${PORT}`);
 });
